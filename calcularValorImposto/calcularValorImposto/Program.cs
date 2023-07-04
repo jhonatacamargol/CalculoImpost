790d017bff515eade9fa68972da947171c07bfc0
jhonatacamargol <jhonatapires32@hotmail.com>
2023-07-03 22:26:33 -0300

    novo commit

---
+++ calcularValorImposto/calcularValorImposto/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Digite o valor do seu salário:");
+int valorS = Convert.ToInt32(Console.ReadLine());
+
+int imposto;
+
+if (valorS <= 2000)
+{
+    imposto = valorS / 100 * 10;
+}
+else if (valorS <= 3500)
+{
+    imposto = valorS / 100 * 13;
+}
+else
+{
+    imposto = valorS / 100 * 15;
+}
+
+Console.WriteLine($"\nO valor do seu imposto é: {imposto} Reais.");
+Console.ReadKey();
